0fd92a3a0c3cd568e2f808fe99f0e6c7b35806da
Yaroslav <YMotalov@gmail.com>
2020-06-23 20:02:52 +0300

    Past some new lessons. Update project.

--- mysite/news/admin.py
+++ mysite/news/admin.py
@@ -1,14 +1,30 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.utils.safestring import mark_safe
+
 from .models import News, Category
 
 class NewsAdmin(admin.ModelAdmin):
-    list_display = ('id', 'title', 'created_at', 'updated_at', 'is_published', 'category')
+    list_display = ('id', 'title', 'created_at', 'updated_at', 'is_published', 'category', 'get_photo')
     list_display_links = ('id', 'title')
     search_fields = ('title', 'content')
     list_editable = ('is_published',)
     list_filter = ('is_published', 'category')
+    fields = ('title', 'category', 'content', 'photo', 'get_photo', 'is_published', 'views', 'created_at', 'updated_at')
+    readonly_fields = ('get_photo', 'views', 'created_at', 'updated_at')
+    save_on_top = True
+
+
+    def get_photo(self, obj):
+        if obj.photo:
+            return mark_safe(f'<img src="{obj.photo.url}" width="75">')
+        else:
+            return '-'
+
+    get_photo.short_description = 'Миниатюра'
+
+
 
 
 class CategoryAdmin(admin.ModelAdmin):
@@ -19,3 +35,5 @@ class CategoryAdmin(admin.ModelAdmin):
 
 admin.site.register(News, NewsAdmin)
 admin.site.register(Category, CategoryAdmin)
+admin.site.site_title = 'Управление новостями'
+admin.site.site_header = 'Управление новостями'
--- mysite/news/views.py
+++ mysite/news/views.py
@@ -10,6 +10,8 @@ class HomeNews(ListView):
     model = News #по сути сделали news = News.objects.all()
     template_name = 'news/home_news_list.html'
     context_object_name = 'news'
+    queryset = News.objects.select_related('Category')
+
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -17,7 +19,7 @@ class HomeNews(ListView):
         return context
 
     def get_queryset(self):
-        return News.objects.filter(is_published=True)
+        return News.objects.filter(is_published=True).select_related('category')
 
 
 # def index(requests):
@@ -42,7 +44,7 @@ class NewsByCategory(ListView):
         return context
 
     def get_queryset(self):
-        return News.objects.filter(category_id=self.kwargs['category_id'], is_published=True)
+        return News.objects.filter(category_id=self.kwargs['category_id'], is_published=True).select_related('category')
 
 
 
